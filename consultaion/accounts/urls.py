89a42adbab8f675e9a4fb552f415a083278809aa
AF Rakib <38295735+rikitoaf@users.noreply.github.com>
2021-03-12 02:25:04 +0600

    login and logout functionality added

--- consultaion/accounts/urls.py
+++ consultaion/accounts/urls.py
@@ -5,5 +5,9 @@ from.import views
 
 
 urlpatterns = [
-    path("",views.home, name="home")
+    path("",views.home, name="home"),
+    
+    path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),  
+	path('logout/', views.logoutUser, name="logout"),
 ]
